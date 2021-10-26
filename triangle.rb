fdaac4049ed38194ac621ec5c641fb1814fe41b3
nortonbatista <nortonbatistaa@gmail.com>
2021-10-26 10:42:25 -0300

    Raise exceptions in invalid triangle sides

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  def test_triangle(a, b, c)
+    sides = [a,b,c]
+
+    sides.each.with_index do |side, index|
+      leftover = sides.select.with_index do |leftoverSide, leftoverIndex|
+        leftoverIndex != index
+      end
+
+      raise TriangleError, "Invalid triangle" unless side > leftover[0] - leftover[1] && side < leftover[0] + leftover[1]
+    end
+  end
+
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "A triangle can't have a side less or equal than 0"
+  end
+
+  test_triangle(a,b,c)
+  
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
