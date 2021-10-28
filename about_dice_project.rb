a94d97dd378384dc636538b7e47c745d447dbafa
nortonbatista <nortonbatistaa@gmail.com>
2021-10-28 16:21:28 -0300

    Fix about_dice_project tests

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,23 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def initialize
+    @values = []
+  end
+
+  def roll(times)
+    @values = []
+    
+    1.upto(times) do |i|
+      @values << rand(1..6)
+    end
+  end
+
+  def values
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
